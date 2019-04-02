fe5c2ea676ac92e5d496a5b0e3d3efe39f010ad6
Welinton Martinez <wmartinez@hdssd.semma.gob.do>
2019-04-02 11:10:16 -0400

    Details de la pelicula hecho

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const moviesRef = firebase.database().ref("Peliculas");
-const apikey = "<--En produccion-->";
-// const apikey = "PlzBanMe";
+// const apikey = "<--En produccion-->";
+const apikey = "PlzBanMe";
 
 function addMovie(data) {
   return moviesRef.push(data);
@@ -13,22 +13,31 @@ function updateMovie(id, data) {
   return moviesRef.child(id).set(data);
 }
 
-function getMovieDetails(id) {
-  //TODO: refactor por la comunidad usando Arrays
-  return new Promise((resolve, reject) => {
-    moviesRef.child(id).once("value", data => {
-      console.log("getMovieDetails", data.val());
-    });
-  });
+function getMovieDetails (id) {
+    //@TODO: Refactor por la comunidad
+    return new Promise ((resolve, reject) => {
+        moviesRef.child(id).once("value", data => {
+            resolve(data.val())
+        })
+    })
 }
+
 
 function getMovieData(title) {
   const url = `http://www.omdbapi.com/?t=${title}&apikey=${apikey}`;
   return fetch(url).then(resp => resp.json());
 }
+function showDetails (data){
+    detailsSlctr.style.display = "block";
+    detailsSlctr.innerHTML = `<pre><code>${JSON.stringify(data, null, 4)}</code></pre>`;
+
+}
+
 
 const filsSlctr = document.getElementById("listPeliculas");
 const titleSlctr = document.getElementById("title");
+const detailsSlctr = document.getElementById("details");
+
 //Eventos
 moviesRef.on("value", data => {
   const peliculaData = data.val();
@@ -40,19 +49,43 @@ moviesRef.on("value", data => {
   for (const key in peliculaData) {
     if (peliculaData.hasOwnProperty(key)) {
       const element = peliculaData[key];
-      cuerpoLista += `<li>${element.Title}</li>`;
+      cuerpoLista += `<li data-id="${key}">${element.Title}
+      <button data-action="details">Detalles</button>
+      <button data-action="edit">Editar</button>
+      <button data-action="delete">Eliminar</button>
+      </li>`;
     }
   }
   filsSlctr.innerHTML = cuerpoLista;
 });
 
-titleSlctr.addEventListener("keyup", event =>{
-    const titleContent = titleSlctr.value.trim();
-    if (event.keyCode === 13 && titleContent) {
-        // console.log("wepa", titleContent);
-        getMovieData(titleContent)
-        .then(movieData => {
-           addMovie(movieData);
-        });
+filsSlctr.addEventListener("click", event => {
+  const target = event.target;
+  if (target.nodeName === "BUTTON") {
+    const id = target.parentNode.dataset.id;
+    const action = target.dataset.action;
+    if(action === "details") {
+        getMovieDetails(id)
+            .then(showDetails);
+    } else if (action === "edit") {
+      const newTitle = prompt("Nuevo Titulo: ").trim();
+      if (newTitle) {
+        getMovieData(newTitle).then(newDetails => updateMovie(id, newDetails));
+      }
+    } else if (action === "delete") {
+      if (confirm("Seguro desea borrar?")) {
+        deleteMovie(id);
+      }
     }
+  }
+  //Viendo las propiedades de ese evento hay una llamada nodeName
+});
+titleSlctr.addEventListener("keyup", event => {
+  const titleContent = titleSlctr.value.trim();
+  if (event.keyCode === 13 && titleContent) {
+    // console.log("wepa", titleContent);
+    getMovieData(titleContent).then(movieData => {
+      addMovie(movieData);
+    });
+  }
 });
