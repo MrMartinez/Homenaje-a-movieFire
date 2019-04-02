7f04e3747fe99c55cd1448295ec54d0507aebf8f
Welinton Martinez <wmartinez@hdssd.semma.gob.do>
2019-04-02 08:13:49 -0400

    Peticion AJAX implementada

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
-console.log('Wepa!'); 
-
-
-const moviesRef = firebase.database().ref('Peliculas');
-
+const moviesRef = firebase.database().ref("Peliculas");
+const apikey = "Indicar Token OJO ;)";
 function addMovie(data) {
-  return moviesRef.push(data)
+  return moviesRef.push(data);
 }
 function deleteMovie(id) {
-   return moviesRef.child(id).remove()
+  return moviesRef.child(id).remove();
 }
 
-function updateMovie(id,data) {
-   return moviesRef.child(id).set(data);
+function updateMovie(id, data) {
+  return moviesRef.child(id).set(data);
 }
 
 function getMovieDetails(id) {
-    return new Promise((resolve, reject)=>{
-        moviesRef.child(id).once("value", data => {
-            console.log("getMovieDetails", data.val());
-    
-        })
-    })
-   
+  return new Promise((resolve, reject) => {
+    moviesRef.child(id).once("value", data => {
+      console.log("getMovieDetails", data.val())
+    });
+  });
 }
 
 function getMovies() {
-    moviesRef.on("value", data => {
-        console.log("getMovies", data.val());
-
-    });
+  moviesRef.on("value", data => {
+    console.log("getMovies", data.val())
+  })
 }
+// TODO: Refactorirzar trabajo conUI directamente
 
-
-// moviesRef.update({
-//     title: "otro documental",
-//     descripcion: "Cambiando un update",
-//     viendoCambio: "todo igual agrego viendo cambio"
-
-// });
+function getMovieData(title) {
+  const url = `http://www.omdbapi.com/?t=${title}&apikey=${apikey}`
+  return fetch(url).then(resp => resp.json())
+}
