4dfe6710de6ea08be43412ebb4a02cf9d173f498
Welinton Martinez <wmartinez@hdssd.semma.gob.do>
2019-04-02 09:17:34 -0400

    Busqueda por input y listado #4

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const moviesRef = firebase.database().ref("Peliculas");
-const apikey = "Indicar Token OJO ;)";
+const apikey = "<--En produccion-->";
+// const apikey = "PlzBanMe";
+
 function addMovie(data) {
   return moviesRef.push(data);
 }
@@ -12,21 +14,45 @@ function updateMovie(id, data) {
 }
 
 function getMovieDetails(id) {
+  //TODO: refactor por la comunidad usando Arrays
   return new Promise((resolve, reject) => {
     moviesRef.child(id).once("value", data => {
-      console.log("getMovieDetails", data.val())
+      console.log("getMovieDetails", data.val());
     });
   });
 }
 
-function getMovies() {
-  moviesRef.on("value", data => {
-    console.log("getMovies", data.val())
-  })
-}
-// TODO: Refactorirzar trabajo conUI directamente
-
 function getMovieData(title) {
-  const url = `http://www.omdbapi.com/?t=${title}&apikey=${apikey}`
-  return fetch(url).then(resp => resp.json())
+  const url = `http://www.omdbapi.com/?t=${title}&apikey=${apikey}`;
+  return fetch(url).then(resp => resp.json());
 }
+
+const filsSlctr = document.getElementById("listPeliculas");
+const titleSlctr = document.getElementById("title");
+//Eventos
+moviesRef.on("value", data => {
+  const peliculaData = data.val();
+  console.log("getMovies: ", peliculaData);
+
+  let cuerpoLista = "";
+  //TODO: refactor por la comunidad usando Arrays
+
+  for (const key in peliculaData) {
+    if (peliculaData.hasOwnProperty(key)) {
+      const element = peliculaData[key];
+      cuerpoLista += `<li>${element.Title}</li>`;
+    }
+  }
+  filsSlctr.innerHTML = cuerpoLista;
+});
+
+titleSlctr.addEventListener("keyup", event =>{
+    const titleContent = titleSlctr.value.trim();
+    if (event.keyCode === 13 && titleContent) {
+        // console.log("wepa", titleContent);
+        getMovieData(titleContent)
+        .then(movieData => {
+           addMovie(movieData);
+        });
+    }
+});
